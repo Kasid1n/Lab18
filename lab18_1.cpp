1eb4e3a0eab917b60af9fa7c18c71cb6b164fff8
kasidin <kasidinkhamfoo@gmail.com>
2025-02-07 18:56:38 +0700

    lab18_1.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,24 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2){
+	double nr1x,nr2x,nr1y,nr2y;
+	nr1x=r1.x+r1.w;
+	nr1y=r1.y-r1.h;
+	nr2x=r2.x+r2.w;
+	nr2y=r2.y-r2.h;
+	double top,left,right,bot;
+	top = min(r1.y,r2.y);
+	bot = max(nr1y,nr2y);
+	left = max(r1.x,r2.x);
+	right = min(nr1x,nr2x);
+	double delx,dely;
+	delx = right-left;
+	dely = top-bot;
 
-
+	if(delx > 0&& dely >0){
+		return delx*dely;
+	}
+	else return 0;
 
 }
